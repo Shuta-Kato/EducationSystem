527a8bf87ddab439c33279bdbbaef98945ad347c
Shota Minamihara <egoistsupercell0@gmail.com>
2024-08-28 01:40:33 +0900

    Update some files

--- database/migrations/2024_08_25_053740_create_curriculums_table.php
+++ database/migrations/2024_08_25_053740_create_curriculums_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('curriculums', function (Blueprint $table) {
             $table->id();
-            $table->string('title',255)->nullable(false)->comment('カリキュラムのタイトルを保存');
+            $table->string('title',255)->nullable(false)->comment('カリキュラムのタイトルを存');
             $table->string('thumbnail',255)->comment('カリキュラムのサムネイル画像を保存');
             $table->text('description');
             $table->text('video_url');
